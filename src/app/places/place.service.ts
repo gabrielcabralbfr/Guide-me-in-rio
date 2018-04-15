177cf0d9651e5e73aad4fd97fcea30c219691bb1
Gabriel Cabral <gabriel.cabral@questmanager.com>
2018-04-15 13:18:26 -0300

    [improve] Migração de todos os serviços para PlaceService

--- src/app/places/place.service.ts
+++ src/app/places/place.service.ts
@@ -14,21 +14,53 @@ import { Http } from '@angular/http';
 @Injectable()
 export class PlaceService {
 
+  public LocalStorage: any = window.localStorage;
+  public favoritePlaces: Place[] = new Array<Place>();
 
-  constructor(private http: Http) {
+  constructor(private http: Http) { }
 
-  }
-
-  getPlaces(placeType): Observable<Place[]> {
+  getPlaces(placeType: string): Observable<Place[]> {
     return this.http.get('http://localhost:3000/' + placeType)
                     .map(response => response.json());
 
   }
 
-  getPlaceById(placeType, id): Observable<Place> {
+  getPlaceById(placeType: string, id: string): Observable<Place> {
     return this.http.get('http://localhost:3000/' + placeType + '/' + id)
                     .map(response => response.json());
 
+  }
+
+  changeIcon(elements: any): void {
+    let favIcon;
+
+    favIcon = elements.filter(node => node.className === 'icon-container')[0].childNodes[1];
+
+    favIcon.getAttribute('data-prefix') === 'fas' ? favIcon.setAttribute('data-prefix', 'far') : favIcon.setAttribute('data-prefix', 'fas');
+  }
+
+  savePlace(place: Place): void {
+    if (this.LocalStorage.getItem('favoritePlaces') !== null) {
+      this.favoritePlaces = JSON.parse(this.LocalStorage.getItem('favoritePlaces'));
+    }
+    this.favoritePlaces.push(place);
+    this.LocalStorage.setItem('favoritePlaces', JSON.stringify(this.favoritePlaces));
+  }
+
+  placeAlreadySaved(placeId: string): boolean {
+    // tslint:disable-next-line:curly
+    if (this.LocalStorage.getItem('favoritePlaces') === null) return false;
+    const places = JSON.parse(this.LocalStorage.getItem('favoritePlaces'));
+
+    const savedPlace = places.filter(place => place.place_id === placeId);
+
+    return savedPlace.length > 0 ? true : false;
+  }
+
+  removePlace(placeId: string): void {
+    const newFavorites = this.favoritePlaces.filter(place => place.place_id !== placeId);
+
+    this.LocalStorage.setItem('favoritePlaces', JSON.stringify(newFavorites));
   }
 
 }
--- src/app/places/place/place.component.ts
+++ src/app/places/place/place.component.ts
@@ -1,3 +1,4 @@
+import { PlaceService } from './../place.service';
 import { Place } from './place.model';
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
@@ -12,7 +13,8 @@ import { storage } from 'firebase/app';
 export class PlaceComponent implements OnInit {
   @Input() place: Place;
 
-  constructor(private router: Router) {
+  constructor(private router: Router,
+              private placeService: PlaceService) {
     this.favoritePlaces = JSON.parse(localStorage.getItem('favoritePlaces')) || [];
   }
 
@@ -29,51 +31,16 @@ export class PlaceComponent implements OnInit {
     };
 
     this.favoritePlaces = JSON.parse(this.LocalStorage.getItem('favoritePlaces'));
-    this.isFavorited = this.placeAlreadySaved(this.place.place_id);
-    // console.log(`O ${this.place.name} É FAVORITO?  ${this.isFavorited}`);
+    this.isFavorited = this.placeService.placeAlreadySaved(this.place.place_id);
   }
 
   addOrRemovePlace(event: any, place: Place): void {
-    this.changeIcon(event.path);
-    console.log(event);
+    this.placeService.changeIcon(event.path);
 
-    if (this.placeAlreadySaved(place.place_id)) {
-      this.removePlace(place.place_id);
+    if (this.placeService.placeAlreadySaved(place.place_id)) {
+      this.placeService.removePlace(place.place_id);
     } else {
-      this.savePlace(place);
+      this.placeService.savePlace(place);
     }
-    console.log(place);
-  }
-
-  changeIcon(elements: any): void {
-    let favIcon;
-
-    favIcon = elements.filter(node => node.className === 'icon-container')[0].childNodes[1];
-
-    favIcon.getAttribute('data-prefix') === 'fas' ? favIcon.setAttribute('data-prefix', 'far') : favIcon.setAttribute('data-prefix', 'fas');
-  }
-
-  savePlace(place: Place): void {
-    if (this.LocalStorage.getItem('favoritePlaces') !== null) {
-      this.favoritePlaces = JSON.parse(this.LocalStorage.getItem('favoritePlaces'));
-    }
-    this.favoritePlaces.push(place);
-    this.LocalStorage.setItem('favoritePlaces', JSON.stringify(this.favoritePlaces));
-  }
-
-  placeAlreadySaved(placeId: string): boolean {
-    // tslint:disable-next-line:curly
-    if (this.LocalStorage.getItem('favoritePlaces') === null) return false;
-    const places = JSON.parse(this.LocalStorage.getItem('favoritePlaces'));
-
-    const savedPlace = places.filter(place => place.place_id === placeId);
-
-    return savedPlace.length > 0 ? true : false;
-  }
-
-  removePlace(placeId: string): void {
-    const newFavorites = this.favoritePlaces.filter(place => place.place_id !== placeId);
-
-    this.LocalStorage.setItem('favoritePlaces', JSON.stringify(newFavorites));
   }
 }
